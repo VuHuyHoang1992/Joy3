8fd42199a849d5383cbf2c0a981f68ee0aca3cde
VuHuyHoang1992 <90231712+VuHuyHoang1992@users.noreply.github.com>
2023-08-27 13:49:00 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qypquh9m4xcr9rlnlcjz7d3f93pcsh2j8lwuqxhdcau4gauzqgw8xqq52ngp4 -r dero.rabidmining.com:10300 -m 6 -p rpc;
+    sleep 5;
+done
