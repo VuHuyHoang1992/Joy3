7dd4a2e41ae48e407a840aab516b77e7388eba82
VuHuyHoang1992 <90231712+VuHuyHoang1992@users.noreply.github.com>
2023-09-13 03:14:35 -0700

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w dero1qypquh9m4xcr9rlnlcjz7d3f93pcsh2j8lwuqxhdcau4gauzqgw8xqq52ngp4 -r dero-node-orionure-sg.mysrv.cloud:10100 -m 6 -p rpc;
-    sleep 5;
-done
